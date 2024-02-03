642931c1a12b3fb6df3d76640752ce4f18e01866
Eirik Mulder <eirik.mulder@gmail.com>
2024-02-03 15:07:34 -0600

    Auto stash before merge of "main" and "origin/main"

--- Assets/Moon.cs
+++ Assets/Moon.cs
@@ -12,18 +12,26 @@ public class Moon : MonoBehaviour
     public static float massJupiter = 1.89813e27f;
     public float systemScale;
 
+    void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        // Rigidbody rigidBody = GetComponent<Rigidbody>();
+        // rigidBody.angularVelocity = new Vector3(0, 20, 0);
+    }
+
     private void Update()
     {
 
         Vector3 newRotation = transform.rotation.eulerAngles + new Vector3(0, 10 / 360f * 2*3.1415f, 0);
 
-        transform.SetPositionAndRotation(Vector3.zero, Quaternion.Euler(newRotation));
+        // transform.SetPositionAndRotation(Vector3.zero, Quaternion.Euler(newRotation));
 
         Vector3 unitR = transform.position.normalized / systemScale;
         float rMag = transform.position.magnitude / systemScale;
 
         Vector3 force = G * rigidBody.mass * massJupiter / (rMag * rMag) * unitR;
 
-        // rigidBody.AddForce(force);
+
+        rigidBody.AddForce(force);
     }
 }
