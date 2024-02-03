31bcb5803e58632b472816db53c66e1bbb4ade9e
Cameron Cohen <cameroncohen2002@gmail.com>
2024-02-03 16:33:55 -0600

    down arrow works

--- Assets/SatelliteController.cs
+++ Assets/SatelliteController.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SatelliteController : MonoBehaviour
+
 {
+    public static int i = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,9 +15,10 @@ public class SatelliteController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey("a"))
+        if (Input.GetKey(KeyCode.DownArrow))
         {
-            Debug.Log("Down arrow");
+            i ++;
+            Debug.Log("Down arrow" + i);
         }
         // Debug.Log("something");
     }
