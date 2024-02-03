5ca5e9bdb0a5c635b3180874b71464fccb940f57
Cameron Cohen <cameroncohen2002@gmail.com>
2024-02-03 17:12:14 -0600

    mom

--- Assets/SatelliteController.cs
+++ Assets/SatelliteController.cs
@@ -6,6 +6,9 @@ public class SatelliteController : MonoBehaviour
 
 {
     private float pitch_rate = 0f;
+    private float yaw_rate = 0f;
+    private float rot_step = 0.002f;
+    private float mom_leak = -0.0001f;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,11 +18,32 @@ public class SatelliteController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.DownArrow))
+        if (Input.GetKey(KeyCode.DownArrow))
         {
-            pitch_rate -= 0.5f;            
+            pitch_rate -= rot_step;            
         }
-        Camera.main.transform.Rotate(new Vector3(pitch_rate, 0, 0));
+        
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            pitch_rate += rot_step;
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            yaw_rate -= rot_step;
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            yaw_rate += rot_step;
+        }
+
+        Camera.main.transform.Rotate(new Vector3(pitch_rate, yaw_rate, 0));
+
+        if (pitch_rate != 0)
+        {
+            pitch_rate = (Mathf.Abs(pitch_rate) + mom_leak) * (Mathf.Abs(pitch_rate) / pitch_rate);
+        }
     }
 
 
