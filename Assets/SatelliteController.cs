2943b7a8589af602bb8dd41720b802bda5b10b4d
Cameron Cohen <cameroncohen2002@gmail.com>
2024-02-03 19:59:02 -0600

    batt dies

--- Assets/SatelliteController.cs
+++ Assets/SatelliteController.cs
@@ -1,5 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
+//using Palmmedia.ReportGenerator.Core.Parser.Analysis;
+//using System.Collections;
+//using System.Collections.Generic;
+//using System.Numerics;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -14,6 +16,7 @@ public class SatelliteController : MonoBehaviour
 
     public float prop_rem;
     private float prop_burn = -.75f;
+
     public float batt_charge;
     private float batt_decay = -1;
 
@@ -21,6 +24,8 @@ public class SatelliteController : MonoBehaviour
 
     private bool prop_out = false;
     private bool batt_out = false;
+
+    private float point_acc;
 
     // Start is called before the first frame update
     void Start()
@@ -44,53 +49,53 @@ public class SatelliteController : MonoBehaviour
             {
                 pitch_rate += rot_step;
                 prop_rem += prop_burn * Time.deltaTime;
-                batt_charge += 2 * batt_decay * Time.deltaTime;
+                batt_charge += 0.25f * batt_decay * Time.deltaTime;
             }
             if (Input.GetKey("w"))
             {
                 pitch_rate -= rot_step;
                 prop_rem += prop_burn * Time.deltaTime;
-                batt_charge += 2 * batt_decay * Time.deltaTime;
+                batt_charge += 0.25f * batt_decay * Time.deltaTime;
             }
 
             if (Input.GetKey("d"))
             {
                 yaw_rate -= rot_step;
                 prop_rem += prop_burn * Time.deltaTime;
-                batt_charge += 2 * batt_decay * Time.deltaTime;
+                batt_charge += 0.25f * batt_decay * Time.deltaTime;
             }
             if (Input.GetKey("a"))
             {
                 yaw_rate += rot_step;
                 prop_rem += prop_burn * Time.deltaTime;
-                batt_charge += 2 * batt_decay * Time.deltaTime;
+                batt_charge += 0.25f * batt_decay * Time.deltaTime;
             }
 
             if (Input.GetKey("e"))
             {
                 roll_rate -= rot_step;
                 prop_rem += prop_burn * Time.deltaTime;
-                batt_charge += 2 * batt_decay * Time.deltaTime;
+                batt_charge += 0.25f * batt_decay * Time.deltaTime;
             }
             if (Input.GetKey("q"))
             {
                 roll_rate += rot_step;
                 prop_rem += prop_burn * Time.deltaTime;
-                batt_charge += 2 * batt_decay * Time.deltaTime;
+                batt_charge += 0.25f * batt_decay * Time.deltaTime;
             }
 
             if (Input.GetKey(KeyCode.UpArrow))
             {
                 prop_rem += 4 * prop_burn * Time.deltaTime;
-                batt_charge += 8 * batt_decay * Time.deltaTime;
+                batt_charge += batt_decay * Time.deltaTime;
             }
             if (Input.GetKey(KeyCode.DownArrow))
             {
                 prop_rem += 4 * prop_burn * Time.deltaTime;
-                batt_charge += 8 * batt_decay * Time.deltaTime;
+                batt_charge += batt_decay * Time.deltaTime;
             }
 
-            batt_charge += batt_decay * Time.deltaTime;
+            batt_charge += 0.125f * batt_decay * Time.deltaTime;
         }
 
         transform.Rotate(new Vector3(pitch_rate, roll_rate, yaw_rate)*Time.deltaTime);
@@ -105,6 +110,7 @@ public class SatelliteController : MonoBehaviour
             batt_charge = 100;
             batt_out = true;
         }
+        if (batt_charge > 100) { batt_charge = 100; }
 
         if (pitch_rate != 0)
         {
@@ -124,6 +130,9 @@ public class SatelliteController : MonoBehaviour
 
         if (prop_out) { Debug.Log("Propellent Out!"); }
         if (batt_out) { Debug.Log("Battery Dead!"); }
+
+        point_acc = Vector3.Dot(transform.up,Vector3.forward);
+        if (point_acc > 0) { batt_charge -= 0.25f * point_acc * batt_decay; }
     }
 
     private void FixedUpdate()
