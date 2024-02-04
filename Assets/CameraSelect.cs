35ae291e3e5f4f7c67a1ecc102941a9fe143c09b
Eirik Mulder <eirik.mulder@gmail.com>
2024-02-04 03:10:12 -0600

    Put cameron's time stuff back

--- Assets/CameraSelect.cs
+++ Assets/CameraSelect.cs
@@ -52,5 +52,13 @@ public class CameraSelect : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.RightShift)) { sat_trail.enabled = !sat_trail.enabled; }
         if (Input.GetKeyDown(KeyCode.RightControl)) { sat_trail.Clear(); }
+
+        if (j < 3 && Input.GetKeyDown(KeyCode.RightArrow)) { j++; }
+        if (j > 0 && Input.GetKeyDown(KeyCode.LeftArrow)) { j--; }
+        if (j == 0) { Time.timeScale = 1; }
+        if (j == 1) { Time.timeScale = 5; }
+        if (j == 2) { Time.timeScale = 20; }
+        if (j == 3) { Time.timeScale = 50; }
+        ts = Time.timeScale;
     }
 }
